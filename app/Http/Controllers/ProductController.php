f45d7acceb5e9b675013a2abfe60de9af1743192
SantyCardo <159256862+SantyCardo@users.noreply.github.com>
2025-10-16 11:52:35 -0500

    Product COntroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    function index()
+    {
+        return "LISTADO DE PRODUCTOS";
+    }
+
+    function create()
+    {
+        return "FORMULARIO DE CREACIÓN DE PRODUCTOS";
+    }
+
+    function show($id, $category = null)
+    {
+        if ($category == null) {
+            return "Deatlle de cada producto: " . $id;
+        } else {
+            return "Deatlle de cada producto: " . $id . " de la categoria: " . $category;
+        }
+    }
 }
