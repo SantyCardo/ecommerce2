406af683b4c06ca271f673000f55d66995dff97a
SantyCardo <159256862+SantyCardo@users.noreply.github.com>
2025-10-16 11:50:31 -0500

    Grupo de Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('products')->controller(ProductController::class)->group(function () {
+    Route::get('/', 'index');
+
+    Route::get('/create',  'create');
+
+    Route::get('/{id}/{category?}', 'show');
 });
